317c4f9c0dd5322444ab62a574e7f0e00ee71f98
Ahmed2120 <ahmed202ashraf202@gmail.com>
2022-10-10 10:03:03 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:grocery_commerce/provider/dark_theme_provider.dart';
+import 'package:grocery_commerce/screens/home_page.dart';
+import 'package:provider/provider.dart';
+
+import 'consts/theme_data.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  DarkThemeProvider darkThemeProvider = DarkThemeProvider();
+
+  getCurrentTheme() async {
+    print('restarted');
+    darkThemeProvider.setDarkTheme =
+        await darkThemeProvider.darkThemePrefs.getTheme();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    getCurrentTheme();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_)=> darkThemeProvider),
+      ],
+      child: Consumer<DarkThemeProvider>(
+        builder: (context, theme, child)=> MaterialApp(
+          title: 'Grocery Commerce',
+          theme: Styles.themeData(theme.getDarkTheme, context),
+          home: const HomePage(),
+        ),
+      ),
+    );
+  }
+}
